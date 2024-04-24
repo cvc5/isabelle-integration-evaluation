22a8637e31c3cee670e490e26fade72e744537e5
Hanna Lachnitt <lachnitt@stanford.edu>
2024-04-24 11:45:20 -0700

    better options

--- cvc5Reconstruction/scripts/getProof.sh
+++ cvc5Reconstruction/scripts/getProof.sh
@@ -2,6 +2,109 @@
 
 source config
 
+benchmark_limit=50
+verbose_mode=false
+verit=false
+cvc5_with_rewrite=false
+cvc5_without_rewrite=false
+stats_dir=misc
+more_stats=false
+delete_timeouts=false
+save_pre_image=false
+
+usage() {
+ echo "Usage: $0 [OPTIONS]"
+ echo "Options:"
+ echo " -h          | --help             Display this help message"
+ echo " -v          | --verbose          Enable verbose mode"
+ echo " -l <nr>     | --batch_size <nr>  Benchmark processing batch size (default: 50)"
+ echo ""
+ 
+ echo "Solver options:"
+ echo " -s <solver> | --solver <solver>  , with <solver> in {verit,cvc5_without_rewrite,cvc5_with_rewrite}"
+ echo "                                  solvers that should be used. Use multiple -s arguments if you want to use more than one solver"
+ echo " -a          | --all              Run all available solvers"
+ echo ""
+ 
+ echo " -d  <dir>   | --out_dir <dir>    name of the directory you want to save the statistic files to (default: saved in misc)"
+ echo " -t   collect additional statistics"
+ echo " -r   delete benchmarks for which at least one solver times out from preprocessed folder"
+ echo " -c   compress and store preprocessed benchmarks in directory"
+ echo ""
+}
+
+handle_options() {
+
+#Reduce long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    '--help')   set -- "$@" '-h'   ;;
+    '--verbose')   set -- "$@" '-v'   ;;
+    '--timeout')   set -- "$@" '-t'   ;;
+    '--no_verit')   set -- "$@" '-s'   ;;    
+    '--no_let')   set -- "$@" '-n'   ;;
+    '--compress')   set -- "$@" '-c'   ;;  
+    '--limit')   set -- "$@" '-l'   ;;
+    *)          set -- "$@" "$arg" ;;
+  esac
+done
+
+while getopts "hvl:s:d:atrc:" flag; do
+ case $flag in
+   h)
+   usage
+   exit 0
+   ;;
+   v)
+   verbose_mode=true
+   ;;
+   l)
+   benchmark_limit=${OPTARG}
+   ;;
+   a)
+   solvers="all solvers: cvc5_with_rewrite, cvc5_without_rewrite, verit"
+   cvc5_without_rewrite=true;
+   cvc5_with_rewrite=true;
+   verit=true;
+   ;;
+   s)
+   solvers+=("$OPTARG")
+   if [ $OPTARG = "cvc5_without_rewrite" ]; then cvc5_without_rewrite=true;
+   elif [ $OPTARG = "cvc5_with_rewrite" ]; then cvc5_with_rewrite=true;
+   elif [ $OPTARG = "verit" ]; then verit=true;
+   else echo "Unsupported Solver! Use one of these: verit, cvc5_without_rewrite, cvc5_with_rewrite."; exit -1; fi
+   ;;
+   d)
+   stats_dir=("$OPTARG")
+   ;;
+   t)
+   more_stats=true;
+   ;;
+   r)
+   delete_timeouts=true;
+   ;;
+   c)
+   save_pre_image=true
+   pre_image_name=${OPTARG}
+   ;;
+   \?)
+   usage
+   exit -1
+   ;;
+ esac
+done
+}
+
+verbose_msg() {
+  if [ "$verbose_mode" = true ] ; then echo $1; fi
+}
+
+write_json(){
+ echo '{"benchmark_name" : "'$1'", "solving": [{"solver_name" : "'$2'", "solving_time" : '$3' '$4'}]}'>> $curr_res_file
+ echo ',' >> $curr_res_file
+}
+
 write_result()
 {
   solver_name=$1
@@ -12,79 +115,168 @@ write_result()
   new_file="${file##*/}"
 
   start_time=$(date +%s%N)
-  output=$(eval "$solver_command")
+  output=$(eval "timeout 10 $solver_command")
   local return_value=$?
   end_time=$(date +%s%N)
 
   if [ $return_value = 124 ] ; 
   then 
-    echo "Timeout"
-    echo "$new_file, $solver_name, timeout" >> $curr_res_file
+    echo "Timeout $file"
+    write_json $new_file $solver_name "-1" "$stats"
+    return 124
   elif [ $return_value = 1 ] ;
   then 
     echo "Solver $solver_name$ could not solve problem!"
-    echo "$new_file, $solver_name, error" >> $curr_res_file
+    write_json $new_file $solver_name "-2" "$stats"
+    return -1
+  elif [[ $output == *"(error "* ]] ;
+    then 
+    echo "Solver $solver_name$ could not solve problem! Some error occurred"
+    write_json $new_file $solver_name "-3" "$stats"
+    return -1
   else
+    # Make proof file
     new_file="${new_file%.*}"  
-    resultFile=$Result_BENCHMARK_HOME$new_file$new_file_ending;
+    resultFile="$Result_BENCHMARK_HOME$solver_name/$new_file.alethe";
     touch $resultFile
     echo "$output">$resultFile;
 
-    #TODO: Copy problem
+    # Copy problem file
+    new_problem_file="$Result_BENCHMARK_HOME$solver_name/$new_file.smt2"
+    cp $file $new_problem_file
+
     elapsed_time=$((end_time - start_time))
-    echo "$new_file, $solver_name, $elapsed_time" >> $curr_res_file
+    
+    # Collect more statistics if wanted
+    if $more_stats ;
+    then
+     file_size=$(stat -c%s "$resultFile")
+     nr_step=$(grep -o '(step' $resultFile | wc -l)
+     stats=', "file_size" : '$file_size', "nr_step" : '$nr_step;
+    fi;
+    write_json $new_file $solver_name $elapsed_time "$stats"
   fi
+  
+  return 0
  
  }
 
-call_cvc5_rewrite()
+call_cvc5_with_rewrite()
 {
   file=$1
-  local cvc5="$CVC5_HOME --proof-format-mode=alethe --dump-proofs --produce-proofs --proof-granularity=dsl-rewrite --full-saturate-quant --no-stats --sat-random-seed=1 --lang=smt2 $file"
-  write_result "cvc5_with_rewrite" "$cvc5" "_rewrite.alethe" $file;
-
+  local cvc5="$CVC5_HOME --proof-format-mode=alethe --dump-proofs --dag-thres=0 --produce-proofs --proof-granularity=dsl-rewrite --proof-define-skolems --full-saturate-quant --no-stats --sat-random-seed=1 --lang=smt2 $file"
+  write_result "cvc5_with_rewrite" "$cvc5" "_rewrite" $file;
+  return $?
 }
 
 call_cvc5_without_rewrite()
 {
   file=$1
-  local cvc5="$CVC5_HOME --proof-format-mode=alethe --dump-proofs --produce-proofs --full-saturate-quant --no-stats --sat-random-seed=1 --lang=smt2 $file"
-  write_result "cvc5_without_rewrite" "$cvc5" "without_rewrite.alethe" $file;
-
+  local cvc5="$CVC5_HOME --proof-format-mode=alethe --dump-proofs --produce-proofs --proof-define-skolems --full-saturate-quant --no-stats --sat-random-seed=1 --lang=smt2 $file"
+  write_result "cvc5_without_rewrite" "$cvc5" "_without_rewrite" $file;
+  return $?
 }
 
+call_veriT()
+{
+  file=$1
+  local verit="$VERIT_HOME --proof-prune --proof-merge --proof-with-sharing --proof-define-skolems --disable-banner --proof=- -s  $file"
+  write_result "verit" "$verit" "_verit" $file;
+  return $?
+}
 
+handle_options "$@"
+
+if [ -z "$solvers" ]; then
+    echo "No solvers set"
+    exit -1
+fi
+
+for val in "${solvers[@]}"; do
+    echo " - $val"
+done
+echo "on the next $benchmark_limit benchmarks"
 
 #Make result directory
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-curr_res_file=$EVAL_RES$current_time".txt"
-#curr_res_dir=$EVAL_RES/$current_time
-#mkdir $curr_res_dir
-#touch $curr_res_dir/res.txt 
+mkdir -p $EVAL_RES$stats_dir/
+mkdir -p $EVAL_RES$stats_dir/"Solver/"
+curr_res_file=$EVAL_RES$stats_dir/"Solver/"$current_time".json"
 touch $curr_res_file
+echo '['>> $curr_res_file
+ 
+dir1="$Result_BENCHMARK_HOME"cvc5_with_rewrite
+dir2="$Result_BENCHMARK_HOME"cvc5_without_rewrite 
+dir3="$Result_BENCHMARK_HOME"verit
+mkdir -p $dir1/
+mkdir -p $dir2/
+mkdir -p $dir3/
+ 
+head -n $benchmark_limit "$PROBLEM_LOG" | while IFS= read -r filename; do
+  file="$PREPROC_BENCHMARK_HOME${filename}"
+  if $verbose_mode ; then echo "Processing: $file"; fi;
 
-#Get benchmark batch limit
-if [ $# -ge 1 ]; then
-    if [ "$1" -ne 0 ]; then
-       echo $1 
-    fi
+  #Run solvers
+  timeout=false
+  error=false
+  
+  if $cvc5_without_rewrite ;
+  then
+    if $verbose_mode ; then echo "Run cvc5 without rewrites..."; fi;
+    call_cvc5_without_rewrite $file
+    retval=$?
+    if [ $retval == 124 ]; then timeout=true; fi;
+    if [ $retval == 255 ]; then error=true; fi;
+  fi
+
+  if $cvc5_with_rewrite ;
+  then
+    if $verbose_mode ; then echo "Run cvc5 with rewrites..."; fi;
+    call_cvc5_with_rewrite $file
+    retval=$?
+    if [ $retval == 124 ]; then timeout=true; fi;
+    if [ $retval == 255 ]; then error=true; fi;
+  fi
+  
+  if $verit ;
+  then
+    if $verbose_mode ; then echo "Run veriT ..."; fi;
+    call_veriT $file
+    call_cvc5_with_rewrite $file
+    retval=$?
+    if [ $retval == 124 ]; then timeout=true; fi;
+    if [ $retval == 255 ]; then error=true; fi;
+  fi
+
+  if $delete_timeouts 
+  then
+    if $timeout || $error ; 
+    then
+      echo "Timeout or Error detected for benchmark $filename, deleting it...";
+      rm $file; 
+    fi;
+  fi;
+done
+
+#Delete the last comma
+if [ $(wc -l < $curr_res_file) -gt 1 ]; then
+    sed -i '$s/.$//' "$curr_res_file"
+fi
+
+echo ']'>> $curr_res_file
+#Delete processed benchmarks from file
+sed -i -e 1,"$benchmark_limit"d $PROBLEM_LOG
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+if $save_pre_image ; then
+ mkdir -p $BENCHMARK_IMAGE_BENCHMARK_HOME$pre_image_name/$current_time/
+ cp -r $Result_BENCHMARK_HOME/* $BENCHMARK_IMAGE_BENCHMARK_HOME$pre_image_name/$current_time/
 fi
 
 
-while IFS= read -r filename; do
-  file="$PREPROC_BENCHMARK_HOME${filename}"
-  echo "Processing: $file"
+if $delete_timeouts 
+then verbose_msg "I deleted files that timed out or gave an error from /preprocessed"; 
+else verbose_msg "I did not delete the files that times out or gave an error /preprocessed"; 
+fi;
 
-  #Run solvers
-  echo "Run cvc5 without rewrites..."
-  call_cvc5_without_rewrite $file
 
-  echo "Run cvc5 with rewrites..."
-  call_cvc5_rewrite $file
-
-  echo "Run veriT..."  
-
-  #If any error was detected save file in Error/ directory and add to Error/log.txt
-
-  #Delete processed benchmarks from file
-done < "$PROBLEM_LOG"
--- cvc5Reconstruction/scripts/preproc.sh
+++ cvc5Reconstruction/scripts/preproc.sh
@@ -3,13 +3,155 @@
 source config
 
 timeout_sec=1
+run_verit=true
+delete_lets=true
+verbose_mode=false
+save_pre_image=false
+limit_amount=false
+delete_bench=false
 
-#For all files that are not marked as sat
-grep -L "(set-info :status sat)" "$INPUT_BENCHMARK_HOME"* | while IFS= read -r file; do
-  #TODO: Use cvc5's timeout? But then I cannot check the exit code I think
-  cvc5=$(timeout $timeout_sec $CVC5_HOME --no-stats --sat-random-seed=1 --lang=smt2 $file)
-  if [ $? -ne 124 ] ; then
-    cp $file $PREPROC_BENCHMARK_HOME
-    echo "Copied $file"
-  fi
+usage() {
+ echo "Usage: $0 [OPTIONS]"
+ echo "Options:"
+ echo " -h       | --help            Display this help message"
+ echo " -v       | --verbose         Enable verbose mode"
+ echo " -t <nr>  | --timeout <nr>    Change timeout limit (default: 1s)"
+ echo " -l <nr>  | --limit <nr>      Only preprocess <nr> benchmarks and then stop (default: all are run). Note that already preprocessed benchmarks are not deleted!"
+ echo " -d       | --delete_bench    This deletes preprocessed benchmarks from /input. Default: false"
+ echo " -s       | --no_verit        Don't run verit (default: false, i.e. veriT is run)"
+ echo " -n       | --no_let          Don't delete lets (default: false, i.e. lets are not deleted)"
+ echo " -c <dir> | --compress <dir>  compress and store preprocessed benchmarks in directory"
+}
+
+handle_options() {
+
+#Reduce long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    '--help')   set -- "$@" '-h'   ;;
+    '--verbose')   set -- "$@" '-v'   ;;
+    '--timeout')   set -- "$@" '-t'   ;;
+    '--limit')   set -- "$@" '-l'   ;;
+    '--delete_bench')   set -- "$@" '-d'   ;;
+    '--no_verit')   set -- "$@" '-s'   ;;    
+    '--no_let')   set -- "$@" '-n'   ;;
+    '--compress')   set -- "$@" '-c'   ;;  
+    *)          set -- "$@" "$arg" ;;
+  esac
 done
+
+while getopts "hvsl:t:c:nd" flag; do
+
+ case $flag in
+   h)
+   usage
+   exit 0
+   ;;
+   v)
+   verbose_mode=true
+   ;;
+   s)
+   run_verit=false
+   ;;
+   n)
+   delete_lets=false
+   ;;
+   l)
+   limit_amount=true
+   run_amount=${OPTARG}
+   limit=$run_amount
+   ;;
+   t)
+   timeout_sec=${OPTARG}
+   ;;
+   d)
+   delete_bench=true
+   ;;
+   c)
+   save_pre_image=true
+   pre_image_name=${OPTARG}
+   ;;
+   \?)
+   usage
+   exit -1
+   ;;
+ esac
+done
+}
+
+write_problem() {
+  if $1;
+  then
+    output_file="$PREPROC_BENCHMARK_HOME$(basename "$file")"
+    cvc5_new_problem=$(timeout $timeout_sec $CVC5_HOME -o raw-benchmark --parse-only --dag-thresh=0 "$file" > "$output_file")
+  else
+    cp $file $PREPROC_BENCHMARK_HOME
+  fi
+}
+
+verbose_msg() {
+  if [ "$verbose_mode" = true ] ; then echo $1; fi
+}
+
+handle_options "$@" 
+
+if $verbose_mode ; then nr_files=$(find "$INPUT_BENCHMARK_HOME" -type f -name "*.smt2" | wc -l); nr_processed=0; fi;
+
+echo $INPUT_BENCHMARK_HOME
+find "$INPUT_BENCHMARK_HOME" -type f -name "*.smt2" | while read -r file; do
+
+  # Check if the file contains the string "(set-info :status sat)"
+  if ! grep -q "(set-info :status sat)" "$file";
+  then cvc5=$(timeout $timeout_sec $CVC5_HOME --no-stats --sat-random-seed=1 --lang=smt2 $file 2>/dev/null)
+    if [ $? -ne 124 ] ;
+    then write_problem $delete_lets
+      if $verbose_mode ; then nr_processed=$(($nr_processed+1)); fi;
+    else
+      if $run_verit ;
+      then
+        #In case cvc5 times out we still want to run veriT in case it can solve the goal to be fair
+        verit=$(timeout $timeout_sec $VERIT_HOME --disable-banner $file)
+        if [ $? -ne 124 ] ;
+        then
+	  write_problem $delete_lets
+          if $verbose_mode ; then nr_processed=$(($nr_processed+1)); fi;
+        fi;
+      fi;
+    fi;
+  fi;
+
+  if $delete_bench ;
+  then
+   rm $file;
+  fi;
+    
+  if $limit_amount ; 
+  then 
+    run_amount=$(($run_amount-1));
+    if [[ $run_amount = 0 ]];
+    then
+      verbose_msg "Finished pre-processing."
+      verbose_msg "Found $nr_files files. "
+      verbose_msg "I preprocessed $limit files."
+      verbose_msg "Of these, "$nr_processed" files remain after preprocessing."; 
+      break;
+    fi;
+  fi;
+done
+  
+
+if [ "$limit_amount" = false ] ;
+then 
+  verbose_msg "Finished pre-processing."
+  verbose_msg "Found $nr_files files. "
+  verbose_msg "I processed all of these. Of these "$nr_processed" files remain after preprocessing."; 
+fi
+
+if $delete_bench 
+then verbose_msg "I deleted all files I preprocessed"; 
+else verbose_msg "I did not delete the files I preprocessed"; 
+fi;
+      
+#Very brittle: Make better
+if $save_pre_image ; then cd $PREPROC_BENCHMARK_HOME; tar -cvf "$PREPROC_IMAGE_BENCHMARK_HOME$pre_image_name.tar" "preprocessed/"; cd $BASE_DIR; fi
