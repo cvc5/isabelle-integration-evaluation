890378abfe321abc532a3db8bff48b93d0c18097
Hanna Lachnitt <lachnitt@barrett2.stanford.edu>
2026-03-03 17:24:17 -0800

    use Isabelle to check external problem and proof

---
+++ reconstruction/scripts/checkOneBench.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+trap "cd \"${PWD}\"" EXIT
+
+#Defaults for options
+timeout=350
+
+Help()
+{
+   # Display Help
+   echo "Run the Isabelle smt-check tool on a proof and problem"
+   echo "Usage: $0 <input problem file> <input proof file>"
+   echo
+   echo "options:"
+   echo "t     Set timeout for solving and producing proof"
+   echo
+}
+
+while getopts ":ht:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      t) timeout=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+shift $((OPTIND - 1))
+
+if [[ "$#" -ne 2 ]]; then
+  Help
+  exit 1
+fi
+
+input_problem_file=$1
+input_proof_file=$2
+
+ISABELLE_PATH=/barrett/scratch/lachnitt/Binaries/dist-Isabelle_24-Feb-2026/Isabelle_24-Feb-2026/bin/
+export USER_HOME=/barrett/scratch/lachnitt/Binaries/IsabelleSetUp/
+
+$ISABELLE_PATH/isabelle smt_check -i $input_problem_file -p $input_proof_file
+
+
